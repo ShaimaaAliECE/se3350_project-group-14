210ee660fc3f42c32541d1e0b608798f546140af
Jacob Bishop <rbishop7@uwo.ca>
2022-02-14 19:09:23 -0500

    dev

--- src/L1/ControlBar.js
+++ src/L1/ControlBar.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import react, {useState} from "react";
 import {sortArray} from "./mergesort.js";
-import {showNext} from './PageHandler.js';
+import {showNext, startGame} from './PageHandler.js';
 
 
 class ControlBar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            
+            'displayStart': true,
+            'displayDirections':false
         }
         
 
     }
 
+   
     render(){
         return (
         <><h1>Mergesort Level 1</h1>
             <div className='tips'>
                 Welcome to SortIt!<br></br>SortIt! will help you learn about important sorting algorithms used by Computer Scientists and Software Engineers.
-            <br></br>To begin, click the <b>START</b> button.</div><div className='stepDescription'>
+            <br></br>To begin, click the <b>START</b> button.</div>
+            <div className='stepDescription'>
 
             </div>
             <div className='controls'> </div>
-            <button id="startBtn" onClick={showNext}>Start</button></>
+            <button id="startBtn" onClick={startGame}>Start</button></>
             
         );
     }
--- src/L1/PageHandler.js
+++ src/L1/PageHandler.js
@@ -1,7 +1,20 @@
 //use either JQUERY or just document selector. IDC
+import jquery from 'jquery';
 let currentStep = 0;
+//how to use JQUERY
+//jquery('.className') or jquery('#idName')
+//jquery('selection').text('new text goes here')
+
 
 
 export function showNext(){
-    console.log(`I would show next but I can't yet.`)
+    console.log(`I would show next but I can't yet.`);
+    jquery('#idname').css('display', 'inline-block');
+    jquery(`#s${currentStep}`).css('display', inline)
+}
+
+export function startGame(){
+    console.log("Game is now starting! Removing all items with ID: startBtn");
+    jquery('#startBtn').remove();
+    jquery('.tips').text(`We just updated the text`);
 }
