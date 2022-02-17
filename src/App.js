2003067e9c372d2c761ee750bb55e2c7376cd03e
Chris <cchen842@uwo.ca>
2022-02-17 16:32:26 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import L1 from "./L1/level";
 import L3 from "./L3/Level";
 import Body from "./components/Body/Body";
-import Error from "./components/ErrorPage/Error";
 
 function App() {
   return (
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,23 +5,35 @@ import { useNavigate } from "react-router-dom";
 
 export default function Body() 
 {
-
+    //useRef's from react
+    const textRefer = useRef();
     const textRef = useRef();
     let nav = useNavigate();
 
+    //use effects for ityped
     useEffect(() => {
-        init(textRef.current, {
-            showCursor: true,
-            backDelay: 1500,
-            backSpeed: 60,
-            strings: ["Julia, Devanshi, Jacob, Vikraj, Faris, Omar, Chris"],
+        init(textRefer.current, {
+            loop: false,
+            showCursor: false,
+            typeSpeed:  200,
+            strings: ["Sort It!"],
         });
     }, []);
 
+    useEffect(() => {
+        init(textRef.current, {
+            loop: false,
+            showCursor: false,
+            startDelay: 2500,
+            strings: ["By: Julia, Devanshi, Jacob, Vikraj, Faris, Omar, Chris"],
+        });
+    }, []);
+
+    //H1 and H2 are the texts that are being typed while The rest are the buttons and joe mama
   return (
     <div className="body">
-        <h1><span className="title">Sort It!</span></h1>
-        <h2>By: <span className="author" ref = {textRef}></span></h2> 
+        <h1><span className="title" ref = {textRefer}></span></h1>
+        <h2><span className="author" ref = {textRef}></span></h2> 
         <button className="button buttons" onClick={() => {nav("/L3/index.js")}}>Play</button>
         <button className="button buttons">Options</button>
         <button className="button buttons">Quit</button>     
